4c916884ef74205978edc36ccbe2741d0b029b12
陈方 <chenfang@yunnex.com>
2018-12-03 15:47:24 +0800

    use logger util

--- mods/traffic_control/main.lua
+++ mods/traffic_control/main.lua
@@ -1,10 +1,16 @@
 --[[
        使用openresty实现基于uri的限流功能
+       nginx配置: 
+       http {
+           lua_shared_dict uri_limit_map 10m;
+           lua_shared_dict qps_limit_store 10m;
+       }
        author: frank chen
 --]]
 local limit_req = require "resty.limit.req"
 local base = require "access_control.mods.base"
 local common_cfg = require "access_control.config"
+local logger = require "access_control.utils.logger"
 
 local uri_limit_map = ngx.shared["uri_limit_map"]
 local redis_key_prefix = "traffic_limit"
@@ -13,38 +19,18 @@ local cache_file_path = common_cfg.cache_file_basepath .. "traffic_limit_setting
 local _M = {}
 setmetatable(_M, {__index = base})
 
-local function log(level, msg)
-    ngx.log(level, "[TXCTRL] ", msg)
-end
-
-local function logInfo(msg)
-    log(ngx.INFO, msg)
-end
-
-local function logError(msg)
-    log(ngx.ERR, msg)
-end
-
-local function logWarn(msg)
-    log(ngx.WARN, msg)
-end
-
-local function logDebug(msg)
-    log(ngx.DEBUG, msg)
-end
-
 --[[
        redis同步配置回调
 --]]
 function _M.on_sync(self)
-    logInfo("Begin update traffic limiting config")
+    logger:info("Begin update traffic limiting config")
 
     -- 从redis或缓存文件读取数据
     local config_list = self:fetch_data(redis_key_prefix, cache_file_path)
 
     -- 更新共享内存
     if config_list == nil or next(config_list) == nil then
-        logWarn("NO CONFIG DATA FOUND FROM REDIS OR CACHE FILE")
+        logger:warn("NO CONFIG DATA FOUND FROM REDIS OR CACHE FILE")
         return
     end
 
@@ -52,13 +38,13 @@ function _M.on_sync(self)
     for k, v in pairs(config_list) do
         local limit = tonumber(v)
         if limit == nil then
-            logWarn("non-number value " .. v .. " for key " .. k)
+            logger:warn("non-number value " .. v .. " for key " .. k)
         else
             uri_limit_map:set(k, limit)
         end
     end
 
-    logInfo("End update traffic limiting config")
+    logger:info("End update traffic limiting config")
 end
 
 --[[
@@ -73,7 +59,7 @@ local function new_limit_req(uri)
     local burst = uri_limit_map:get(uri) * 0.1
     local result, err = limit_req.new("qps_limit_store", qps_limit, burst)
     if not result then
-        logError("failed to instantiate a resty.limit.req object: " .. err .. ", uri: " .. uri)
+        logger:err("failed to instantiate a resty.limit.req object: " .. err .. ", uri: " .. uri)
         return nil
     end
     return result
@@ -95,13 +81,13 @@ function _M.on_filter(self)
             -- where the throttling goes
             return ngx.exit(503)
         end
-        logError("failed to limit req: " .. err)
+        logger:err("failed to limit req: " .. err)
         return ngx.exit(500)
     end
 
     if delay >= 0.001 then
         -- 当 $qps_limit < QPS < qps_limit + $burst时, 为了保持QPS == $qps_limit，这里delay一段时间
-        logDebug("delay it here a bit to conform to the connection limit, time: " .. delay)
+        logger:debug("delay it here a bit to conform to the connection limit, time: " .. delay)
         ngx.sleep(delay)
     end
 end
